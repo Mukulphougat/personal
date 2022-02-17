eb6b8103fda6251fb02c72d21a8baeb04fe48a81
Mukul Phougat <mukulphoughat@gmail.com>
2022-02-17 15:46:10 +0530

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import './styles.css';
 function Header() {
     return (
         <div class="header">
-            <a href="https://www.linkedin.com/in/mukul-phougat-602657193/" class="resume" target="_blank">Resume</a>
+            <a href="https://docs.google.com/document/d/1QtegV3zpQXWDFAhVjPtTzeSU1aokL4m4/edit?usp=sharing&ouid=109608711446096127359&rtpof=true&sd=true" class="resume" target="_blank">Resume</a>
             <a href = "mailto: mukulphoughat@gmail.com" class="email">Send Email</a>
         </div>
     )
