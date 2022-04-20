efbf040c035c12c9a47c1acd05d92c7403075f56
Mukul <mukulphoughat@gmail.com>
2022-04-20 09:53:31 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import './styles.css';
 function Header() {
     return (
         <div class="header">
-            <a href="https://docs.google.com/document/d/1QtegV3zpQXWDFAhVjPtTzeSU1aokL4m4/edit?usp=sharing&ouid=109608711446096127359&rtpof=true&sd=true" class="resume" target="_blank">Resume</a>
+            <a href="https://drive.google.com/file/d/1ie1p-jVMvLwkxBnZCjnuOPHhsNMYAsQg/view?usp=sharing" class="resume" target="_blank">Resume</a>
             <a href = "mailto: mukulphoughat@gmail.com" class="email">Send Email</a>
         </div>
     )
